d50b0109a0ce9305f1cfc6061ea01910c954e794
Misir Jafarov <misir.ceferov@gmail.com>
2023-03-28 20:01:17 +0400

    A bunch of updates

---
+++ README.md
@@ -0,0 +1,13 @@
+## DinDspection
+
+An experiment for running docker in docker in docker in ... in docker.
+
+## Why?
+
+Just to see if I can. (spoiler alert: I can!)
+
+## Run
+
+```sh
+docker run --privileged ghcr.io/themisir/dindspection:main
+```
---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+sudo dockerd --host tcp://0.0.0.0:2375 --tls=false >> /dev/null &
+
+sleep 3 # wait for dockerd to come online
+docker -H tcp://localhost:2375 run hello-world
+docker -H tcp://localhost:2375 run --privileged ghcr.io/themisir/dindspection:main
