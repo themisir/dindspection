8afc4e4d4b5b5c7e471225d16e0138bc96c68ec3
Misir Jafarov <misir.ceferov@gmail.com>
2023-03-28 20:05:33 +0400

    Forward all logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-sudo dockerd --host tcp://0.0.0.0:2375 --tls=false >> /dev/null &
+sudo dockerd --host tcp://0.0.0.0:2375 --tls=false &> /dev/null &
 
 sleep 3 # wait for dockerd to come online
 docker -H tcp://localhost:2375 run hello-world
